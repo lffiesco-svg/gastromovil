b5b3fba98dc06b17484a5bfe1f0bfd443413e47d
Oscar Ospina <ospinacadenaoscar@gmail.com>
2026-03-25 18:04:57 -0500

    fix: corregir chatbot URL, JSON, toggleChat y actualizar gitignore

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ChatView
-
+ 
 urlpatterns = [
-    path('chat/', ChatView.as_view(), name='chat'),
+    path('chatbot/', ChatView.as_view(), name='chatbot'),
 ]
