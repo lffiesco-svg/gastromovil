85809dc78141c731c0caf02d6ed952e003c1c575
Luisfelhunter27 <lffiesco@gmail.com>
2026-04-09 13:37:37 -0500

    wip: guardando cambios antes de cambiar de rama

--- core/views.py
+++ core/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html')
 
 # AUTH
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('perfil')
+        else:
+            return render(request, 'auth/login.html', {'error': 'Credenciales incorrectas'})
     return render(request, 'auth/login.html')
 
 def register_view(request):
     return render(request, 'auth/register.html')
 
+@login_required(login_url='login')
 def perfil(request):
-    return render(request, 'auth/perfil.html')
+    return render(request, 'auth/perfil.html', {'usuario': request.user})
 
 # RESTAURANTES
 def restaurantes(request):
@@ -75,3 +86,9 @@ def verificar_codigo(request):
 
 def admin_panel(request):
     return render(request, 'paneles/admin_panel.html')
+
+def panel_repartidor(request):
+    return render(request, 'paneles/panel_repartidor.html')
+
+def panel_restaurante(request):
+    return render(request, 'paneles/panel_restaurante.html')
