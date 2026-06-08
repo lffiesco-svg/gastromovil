e99f9bb269bcf5c0162e0f835e4952db81cb88b3
Luisfelhunter27 <lffiesco@gmail.com>
2026-06-08 12:20:11 -0500

    estilos

--- gastromovil/settings.py
+++ gastromovil/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'core',
     'rest_framework',
     'carrito',
